55e9562c74e7c483e360c28e78b315f92bf42144
Simo Siren <sirensimo7@gmail.com>
2022-10-25 10:51:44 +0300

    listaus ja lisäys ok

--- RestapiDath/Controllers/PelitController.cs
+++ RestapiDath/Controllers/PelitController.cs
@@ -11,11 +11,21 @@ namespace RestapiDath.Controllers
         // Tietokantakonteksti
         PeliDBContext db = new PeliDBContext();
 
+        // Kaikkien pelien haku
         [HttpGet]
         public ActionResult GetAll()
         {
             var pelit = db.Pelits.ToList();
             return Ok(pelit);
+        }
+
+        // Uuden pelin lisääminen
+        [HttpPost]
+        public ActionResult AddNew([FromBody] Pelit p)
+        {
+            db.Pelits.Add(p);
+            db.SaveChanges();
+            return Ok("Lisättiin uusi peli: " + p.Nimi);
         }
 
     }
